0c43620108dd5bbc10a26f89cd1e2389e6390de5
Morgan Fudge <mln_mf@hotmail.com>
2017-11-24 16:11:53 -0330

    Added basic specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-require "bundler/setup"
-require "top_games"
+require 'bundler/setup'
+require 'top_games'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
--- spec/top_games_spec.rb
+++ spec/top_games_spec.rb
@@ -1,5 +1,39 @@
+require 'spec_helper'
+include TopGames
+
 RSpec.describe TopGames do
-  it "has a version number" do
-    expect(TopGames::VERSION).not_to be nil
+  it 'has a version number' do
+    expect(VERSION).not_to be nil
+  end
+
+  describe 'Games' do
+    game = Game.new('Test', 'http://store.steampowered.com/app/271590/Grand_Theft_Auto_V/')
+
+    it 'has a name' do
+      expect(game.name).to eq('Test')
+    end
+
+    it 'has a url' do
+      expect(game.url).to eq('http://store.steampowered.com/app/271590/Grand_Theft_Auto_V/')
+    end
+  end
+
+  describe 'Scraper' do
+    Game.clear
+    scraper = Scraper.new
+    steam_data = scraper.scrape_steam_chart
+
+    it 'scrapes data from steam' do
+      expect(steam_data).to be_a(Array)
+      expect(steam_data[0]).to be_a(Game)
+      expect(steam_data[0].name).to_not eq('')
+      expect(steam_data[0].url).to_not eq('')
+    end
+  end
+
+  describe 'CLI' do
+    it 'should exist for now' do
+      expect(CLI).not_to be nil
+    end
   end
 end
