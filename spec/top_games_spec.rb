c5e827fc33c99bf9189ee6fe9ddffb32a1d035ac
Morgan Fudge <mln_mf@hotmail.com>
2017-12-06 19:13:53 -0330

    Updated specs

--- spec/top_games_spec.rb
+++ spec/top_games_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe TopGames do
   end
 
   describe 'Games' do
-    game = Game.new('Test', 'http://store.steampowered.com/app/271590/Grand_Theft_Auto_V/')
+    game = Game.new('Test', 'http://store.steampowered.com/app/271590/Grand_Theft_Auto_V/', 5.0)
 
     it 'has a name' do
       expect(game.name).to eq('Test')
@@ -16,24 +16,46 @@ RSpec.describe TopGames do
     it 'has a url' do
       expect(game.url).to eq('http://store.steampowered.com/app/271590/Grand_Theft_Auto_V/')
     end
+
+    it 'has a price' do
+      expect(game.price).to be_a(Float)
+    end
   end
 
   describe 'Scraper' do
     Game.clear
-    scraper = Scraper.new
-    steam_data = scraper.scrape_steam_chart
+    steam_data = Scraper.new.scrape_steam_chart
+    game = Game.all.first.fetch_detailed
 
-    it 'scrapes data from steam' do
+    it 'should scrape data from steam' do
       expect(steam_data).to be_a(Array)
       expect(steam_data[0]).to be_a(Game)
       expect(steam_data[0].name).to_not eq('')
       expect(steam_data[0].url).to_not eq('')
+      expect(steam_data[0].price).to be_a(Float)
+    end
+
+    it 'should scrape detailed game data' do
+      expect(game.tags).to be_a(String)
+      expect(game.rating).to be_a(String)
     end
   end
 
   describe 'CLI' do
-    it 'should exist for now' do
-      expect(CLI).not_to be nil
+    cli = CLI.new
+    cli.refresh_games
+    cli.games.first.fetch_detailed
+
+    it('should have a list of games') do
+      expect(cli.games).to be_a(Array)
+      expect(cli.games.first).to be_a(Game)
+    end
+
+    it('should accept multiple user inputs') do
+      expect { cli.switch('help') }.to output(/Lists the current top sellers/).to_stdout
+      expect { cli.switch('list') }.to output(/1. /).to_stdout
+      expect { cli.switch('1') }.to output(/URL/).to_stdout
+      expect(cli.switch('quit')).to eq(true)
     end
   end
 end
