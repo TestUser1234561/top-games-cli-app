8bbe085b2af0e9fe8e8ae960c4bea71c35202481
Morgan Fudge <mln_mf@hotmail.com>
2017-12-05 21:26:20 -0330

    Added detailed game scraping via steam

--- lib/top_games/game.rb
+++ lib/top_games/game.rb
@@ -2,7 +2,8 @@ module TopGames
   # Container for game information
   class Game
 
-    attr_accessor :name, :price, :discount, :before_discount, :url, :description, :rating, :tags, :developer, :publisher
+    attr_accessor :name, :price, :discount, :before_discount, :url, :description, :rating, :tags, :developer,
+                  :publisher, :bundle
 
     @all = []
 
@@ -15,16 +16,27 @@ module TopGames
       self.class.all << self
     end
 
+    # Update games detailed data
+    def fetch_detailed
+      Scraper.new.scrape_steam_detailed(self)
+    end
+
     # Returns all game objects in memory
     # @return [Array] An array of {Game games's}
     def self.all
       @all
     end
 
-    # CLear all game's im memory
+    # Clear all game's im memory
     # @return [Array] An empty array
     def self.clear
       @all.clear
+    end
+
+    # Update every game's detailed data
+    # @return [Array] An array of {Game games's}
+    def self.update_all
+      @all.each { |game| game.fetch_detailed }
     end
 
   end
--- lib/top_games/scraper.rb
+++ lib/top_games/scraper.rb
@@ -2,6 +2,13 @@ module TopGames
   # Scrapes web for current top selling games
   class Scraper
 
+    attr_accessor :agent
+
+    def initialize
+      @agent = Mechanize.new
+      @agent.cookie_jar.load("#{__dir__}/cookies.yaml")
+    end
+
     # Scrape steam charts for game data
     # @param url [String] web page URL
     # @return [Array] An array of {Game games's}
@@ -22,6 +29,33 @@ module TopGames
       end
 
       Game.all
+    end
+
+    # Scrape detailed game data from steam
+    # @param game [Game] web page URL
+    # @return [Game] A {Game}
+    def scrape_steam_detailed(game)
+      return game.bundle = true unless game.url.to_s.include?('app')
+
+      page = agent.get(game.url)
+
+      page = bypass_steam_age_check(page) if page.uri.to_s.include?('agecheck')
+      puts page.uri
+      description = page.css('div.game_description_snippet')
+      rating = page.css('div.summary span')
+      game.description = description.text.strip unless description.nil?
+      game.rating = rating.first.text.strip unless rating.nil?
+      game.tags = page.css('div.glance_tags a.app_tag').collect { |tag| tag.text.strip }.join(', ')
+      game.developer = page.css('#developers_list a').text
+      game.publisher = page.css('.user_reviews .dev_row a').last.text
+    end
+
+    # Bypass steam age check verification
+    # @param page [Mechanize::Page] Verification page
+    # @return [Mechanize::Page] Game page
+    def bypass_steam_age_check(page)
+      form = page.forms.last
+      @agent.submit(form)
     end
 
   end
