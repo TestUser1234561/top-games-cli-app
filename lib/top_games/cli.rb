a9ea1b51e452e6ac015fbebebaddc7af19f266ae
Morgan Fudge <mln_mf@hotmail.com>
2017-12-05 21:27:02 -0330

    Added individual game printouts to CLI

--- lib/top_games/cli.rb
+++ lib/top_games/cli.rb
@@ -2,17 +2,13 @@ module TopGames
   # A simple command line interface for using the top games gem
   class CLI
 
-    attr_accessor :games
+    attr_accessor :games, :games_sorted
 
     # Displays a list of current top selling games
     def chart(games = @games)
-      games.each.with_index(1) { |game, n| puts "#{n}. #{game.name} - #{game.price}" }
-    end
-
-    # Refreshes the current game list
-    def refresh
-      Game.clear
-      @games = Scraper.new.scrape_steam_chart
+      @games_sorted = nil if games.object_id == @games.object_id
+      puts ''
+      games.each.with_index(1) { |game, n| puts "#{n}. #{game.name}" }
     end
 
     # Displays a sorted list by price or rating
@@ -20,29 +16,66 @@ module TopGames
     def sort(arg)
       case arg
         when 'price'
-          chart(@games.sort_by(&:price))
+          chart(@games_sorted = @games.sort_by(&:price))
         else
           puts "Sort method not recognized, type 'help' to get a list of commands."
       end
+    end
+
+    # Prints more detailed data for a specific game
+    # @param num [Integer]
+    def print_detailed(num)
+      if num.between?(1, @games.count)
+        game = @games[num - 1]
+        game = @games_sorted[num - 1] unless @games_sorted.nil?
+
+        puts "-----| #{game.name} |-----"
+        puts 'Game Bundle!' if game.bundle
+        puts "Price          :    #{game.price}"
+        unless game.bundle
+          puts "Description    :    #{game.description}" unless game.description.empty?
+          puts "Rating         :    #{game.rating}"
+          puts "Developer      :    #{game.developer}"
+          puts "Publisher      :    #{game.publisher}"
+          puts "Tags           :    #{game.tags}"
+        end
+        puts "URL            :    #{game.url}"
+      else
+        puts 'Game not found. Please check the number entered.'
+      end
+    end
+
+    # Refresh the current game list
+    def refresh_games
+      Game.clear
+      @games = Scraper.new.scrape_steam_chart
     end
 
     # Calls CLI methods from strings
     # @param input [String] a CLI command
     def switch(input)
       input = input.split(' ')
-      case input[0]
-        when 'list'
-          puts ''
-          input.count > 1 ? sort(input[1]) : chart
-        when 'help'
-          puts 'list        :    Lists the current top sellers'
-          puts 'list [sort] :    Displays the list stored by (price, rating)'
-          puts 'quit        :    Quits the app'
-        when 'quit'
-          return true
+
+      return true if input[0] == 'quit'
+
+      if input[0] == 'list'
+        if input.count > 1
+          sort(input[1])
         else
-          puts "Please check your input or use 'help' to get a list of commands!"
+          @games_sorted = nil
+          chart
+        end
+      elsif input[0].to_i > 0
+        print_detailed(input[0].to_i)
+      elsif input[0] == 'help'
+        puts 'list        :    Lists the current top sellers'
+        puts 'list [sort] :    Displays the list stored by (price, rating)'
+        puts '[#]         :    Displays extra details about selected game'
+        puts 'quit        :    Quits the app'
+      else
+        puts "Please check your input or use 'help' to get a list of commands!"
       end
+
       false
     end
 
@@ -50,9 +83,12 @@ module TopGames
     # @return [Object] {CLI}
     def self.start
       cli = new
-      cli.refresh
       puts '--------| Top Selling Games |--------'
-      puts "Type 'help' to get a list of commands!"
+      puts 'Please wait for game list to load...'
+      cli.refresh_games
+      Game.update_all
+      cli.chart
+      puts "Enter a number to select a game or type 'help' to get a list of commands!"
       loop { break if cli.switch(gets.chomp.downcase) }
       cli
     end
